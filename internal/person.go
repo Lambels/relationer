59763074a56f4a5258b5a1ff32d675d45394c7a4
Lambels <patrick.arvatu@yahoo.com>
2022-04-29 19:42:22 +0300

    added validation

--- internal/person.go
+++ internal/person.go
@@ -1,9 +1,18 @@
 package internal
 
-import "time"
+import (
+	"time"
+)
 
 type Person struct {
 	ID        int64     `json:"id"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"createdAt"`
+}
+
+func (p *Person) Validate() error {
+	if p.Name == "" {
+		return Errorf(EINVALID, "name is a required field")
+	}
+	return nil
 }
