ce437505790421be9d8f76f62c974be4a3be1b7f
Lambels <patrick.arvatu@yahoo.com>
2022-04-29 19:41:53 +0300

    init commit

---
+++ internal/friendship.go
@@ -0,0 +1,18 @@
+package internal
+
+// Friendship represents a friendship from the perspective of P1.
+type Friendship struct {
+	P1   *Person   `json:"p1"`
+	With []*Person `json:"with"`
+}
+
+func (f Friendship) Validate() error {
+	if f.P1 == nil {
+		return Errorf(EINVALID, "nil pointer")
+	}
+
+	if len(f.With) == 0 {
+		return Errorf(EINVALID, "at least one person is required")
+	}
+	return nil
+}
