ae7633294ace98bbfb538e29300ea39e2bd85640
Lambels <patrick.arvatu@yahoo.com>
2022-05-07 09:38:52 +0300

    req/resp types

--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -4,6 +4,19 @@ type ErrorResponse struct {
 	Error string `json:"error"`
 }
 
+type GetDepthResponse struct {
+	Depth int `json:"depth"`
+}
+
 type RemovePersonRequest struct {
+	Id int64 `json:"id"`
+}
+
+type GetDepthRequest struct {
+	Id1 int64 `json:"id1"`
+	Id2 int64 `json:"id2"`
+}
+
+type GetFriendshipRequest struct {
 	Id int64 `json:"id"`
 }
