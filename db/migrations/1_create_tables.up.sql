f36ea502057492791b81d1e73cbf17bef2486bf6
Lambels <patrick.arvatu@yahoo.com>
2022-05-08 16:59:21 +0300

    db schema

---
+++ db/migrations/1_create_tables.up.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+CREATE TABLE people (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    created_at date NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE friendships (
+    person1_id int REFERENCES people (id) ON DELETE CASCADE,
+    person2_id int REFERENCES people (id) ON DELETE CASCADE
+);
+
+COMMIT;
