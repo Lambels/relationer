e52336d666a6f0725b59384b03ffd86c7aad1202
Lambels <patrick.arvatu@yahoo.com>
2022-05-11 06:37:35 +0300

    highlevel implementation

--- internal/postgresql/store.go
+++ internal/postgresql/store.go
@@ -2,7 +2,6 @@ package postgresql
 
 import (
 	"context"
-	"database/sql"
 
 	"github.com/Lambels/relationer/internal"
 	"github.com/Lambels/relationer/internal/service"
@@ -11,25 +10,69 @@ import (
 // PostgreSqlStoreService
 type PostgreSqlStoreService struct {
 	cache service.Cache
-	db    *sql.DB
+	db    *DB
+}
+
+func NewPostgresqlStore(db *DB, cache service.Cache) *PostgreSqlStoreService {
+	return &PostgreSqlStoreService{
+		cache: cache,
+		db:    db,
+	}
 }
 
 // AddPerson
 func (s *PostgreSqlStoreService) AddPerson(ctx context.Context, person *internal.Person) error {
+	tx, err := s.db.BeginTX(ctx, nil)
+	if err != nil {
+		return internal.WrapError(err, internal.EINTERNAL, "db.BeginTX")
+	}
+	defer tx.Rollback()
 
+	if err := addPerson(ctx, tx, person); err != nil {
+		return parsePostgreErr(err)
+	}
+
+	return internal.WrapErrorNil(tx.Commit(), internal.EINTERNAL, "tx.Commit")
 }
 
 // AddFriendship
 func (s *PostgreSqlStoreService) AddFriendship(ctx context.Context, friendship internal.Friendship) error {
+	tx, err := s.db.BeginTX(ctx, nil)
+	if err != nil {
+		return internal.WrapError(err, internal.EINTERNAL, "db.BeginTX")
+	}
+	defer tx.Rollback()
 
+	if err := addFriendship(ctx, tx, friendship); err != nil {
+		return parsePostgreErr(err)
+	}
+
+	return internal.WrapErrorNil(tx.Commit(), internal.EINTERNAL, "tx.Commit")
 }
 
 // RemovePerson
 func (s *PostgreSqlStoreService) RemovePerson(ctx context.Context, id int64) error {
+	tx, err := s.db.BeginTX(ctx, nil)
+	if err != nil {
+		return internal.WrapError(err, internal.EINTERNAL, "db.BeginTX")
+	}
+	defer tx.Rollback()
+
+	if err := removePerson(ctx, tx, id); err != nil {
+		return parsePostgreErr(err)
+	}
+
+	return internal.WrapErrorNil(tx.Commit(), internal.EINTERNAL, "tx.Commit")
+}
+
+func addPerson(ctx context.Context, tx *Tx, person *internal.Person) error {
 
 }
 
-// GetPerson
-func (s *PostgreSqlStoreService) GetPerson(ctx context.Context, id int64) (internal.Person, error) {
+func addFriendship(ctx context.Context, tx *Tx, friendship internal.Friendship) error {
+
+}
+
+func removePerson(ctx context.Context, tx *Tx, id int64) error {
 
 }
