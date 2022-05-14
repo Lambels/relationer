51f6a444a8bf29aa22f41b997faa7f3590bc53a2
Lambels <patrick.arvatu@yahoo.com>
2022-05-14 17:22:26 +0300

    removed unused request types + helper funcs

--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -1,5 +1,13 @@
 package rest
 
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/Lambels/relationer/internal"
+)
+
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
@@ -8,15 +16,33 @@ type GetDepthResponse struct {
 	Depth int `json:"depth"`
 }
 
-type RemovePersonRequest struct {
-	Id int64 `json:"id"`
+type idKey struct{}
+
+func sendErrorResponse(w http.ResponseWriter, err error) {
+	var ierr *internal.Error
+	var errResp ErrorResponse
+	var status int
+
+	if !errors.As(err, &ierr) {
+		errResp.Error = "internal server error"
+		status = http.StatusInternalServerError
+	} else {
+		errResp.Error = ierr.Error()
+		status = internal.StatusCodeFromECode(ierr.Code())
+	}
+
+	sendResponse(w, errResp, status)
 }
 
-type GetDepthRequest struct {
-	Id1 int64 `json:"id1"`
-	Id2 int64 `json:"id2"`
-}
+func sendResponse(w http.ResponseWriter, res interface{}, status int) {
+	w.Header().Set("Content-Type", "application/json")
 
-type GetFriendshipRequest struct {
-	Id int64 `json:"id"`
+	payload, err := json.Marshal(res)
+	if err != nil { // send json this way to separate encoding error (can call w.WriteHeader only once)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(status)
+	w.Write(payload)
 }
