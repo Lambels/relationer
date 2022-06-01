ad57a79a3718e9ab40903883f5b577b16ad771e3
Lambels <patrick.arvatu@yahoo.com>
2022-06-01 11:36:59 +0300

    added some rest test cases

--- client/client_test.go
+++ client/client_test.go
@@ -2,9 +2,87 @@ package client
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
+
+	"github.com/Lambels/relationer/internal"
 )
+
+func TestAddPerson(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		buf, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var data map[string]interface{}
+		if err := json.Unmarshal(buf, &data); err != nil {
+			t.Fatal(err)
+		}
+
+		if got, want := data["name"], "foo"; got != want {
+			t.Fatalf("Got: %v Want: %v", got, want)
+		}
+
+		data["id"] = 1
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(data)
+	}))
+	defer ts.Close()
+
+	c := New(&ClientConfig{
+		URL:            ts.URL,
+		Client:         http.DefaultClient,
+		ConsumerConfig: nil,
+	})
+	id, err := c.AddPerson(context.Background(), "foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := id, int64(1); got != want {
+		t.Fatalf("Got: %v Want: %v", got, want)
+	}
+}
+
+func TestAddFriendship(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		buf, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var data internal.Friendship
+		if err := json.Unmarshal(buf, &data); err != nil {
+			t.Fatal(err)
+		}
+
+		if got, want := data.P1.ID, int64(1); got != want {
+			t.Fatalf("Got: %v Want: %v", got, want)
+		}
+
+		if got, want := data.With[0], int64(2); got != want {
+			t.Fatalf("Got: %v Want: %v", got, want)
+		}
+
+		w.WriteHeader(http.StatusCreated)
+	}))
+	defer ts.Close()
+
+	c := New(&ClientConfig{
+		URL:            ts.URL,
+		Client:         http.DefaultClient,
+		ConsumerConfig: nil,
+	})
+	if err := c.AddFriendship(context.Background(), 1, 2); err != nil {
+		t.Fatal(err)
+	}
+}
 
 func TestCtxCancelDetached(t *testing.T) {
 	c := New(nil)
